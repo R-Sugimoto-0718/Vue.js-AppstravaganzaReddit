892e3bb06c2ce322788da9bffad40d8703cf1b10
waspryo <wasp2123321@gmail.com>
2019-12-14 17:09:52 +0900

    get the data from  rarepuppers

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  name: "Posts"
+  name: 'Posts',
 };
 </script>
 
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -9,7 +9,22 @@ export default {
   name: 'Posts',
   data() {
     return {
+      posts: []
     };
+  },
+  mounted() {
+    this.load();
+  },
+  methods: {
+    load() {
+      // fetch the post
+      const url = 'https://www.reddit.com/r/rarepuppers/.json';
+      fetch(url)
+        .then(res => res.json())
+        .then((result) => {
+          this.posts = result.data.children;
+        });
+    },
   },
 };
 </script>
