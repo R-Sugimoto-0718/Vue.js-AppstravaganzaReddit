d4a62f6573c21a26c0ed229fcf9e2858f8b32c65
waspryo <wasp2123321@gmail.com>
2019-12-14 16:55:52 +0900

    changed url and add boostwatch

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="Posts">
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">/r/rareppuers</a>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "Posts"
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>posts</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
