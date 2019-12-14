c7192c501e7252c478a59b2375a3a2d166dd80a6
waspryo <wasp2123321@gmail.com>
2019-12-14 17:15:44 +0900

    add media object by bootstrap

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="hello">
-    <h1>posts</h1>
+  <div class="hello mt-3">
+    <ul class="list-unstyled">
+      <li class="media">
+        <img src="">
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">List-based media object</h5>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,7 +17,7 @@ export default {
   name: 'Posts',
   data() {
     return {
-      posts: []
+      posts: [],
     };
   },
   mounted() {
