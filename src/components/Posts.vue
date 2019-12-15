88a5ca429256f92f4677bab906116765b2559b4c
waspryo <wasp2123321@gmail.com>
2019-12-15 13:14:31 +0900

    create Url

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -4,9 +4,10 @@
       <li v-for="post in posts" class="media m-3">
         <img :src="post.data.thumbnail" class="mr-3" alt="">
         <div class="media-body">
-          <h5 class="mt-0 mb-1">{{ post.data.title }}</h5>
+          <h5 class="mt-0 mb-1"><a href="createUrl(post.data.permalink)" target="_blank">{{ post.data.title }}</a></h5>
           <p>
             <h3 class="text-danger">{{ post.data.ups }} 👆</h3>
+            <p>created {{ post.data.created }} ago by {{ post.data.author }}</p>
             <span class="badge badge-pill badge-primary">{{ post.data.num_comments }} comments</span>
           </p>
         </div>
@@ -16,6 +17,7 @@
 </template>
 
 <script>
+import { parse, distanceInWords } from 'date-fns'
 export default {
   name: 'Posts',
   data() {
@@ -36,6 +38,9 @@ export default {
           this.posts = result.data.children;
         });
     },
+    createUrl(path) {
+      return `https://www.reddit.com${path}`
+    }
   },
 };
 </script>
